5b4174f869d6c8ab77bd2b7f590cda64b67f7f15
ishmaifan <ishma07777@gmail.com>
2024-08-09 15:21:18 +0530

    new release and popular added.

--- index.js
+++ index.js
@@ -185,3 +185,19 @@ let movies = [
   }, 3000);
   
   
+  const videoCards = [...document.querySelectorAll('.video-card')]
+
+  videoCards.forEach(item =>{
+    item.addEventListener('mouseover', () =>{
+      let video = item.children[1];
+      video.play();
+    })
+    item.addEventListener('mouseleave',()=>{
+      let video = item.children[1];
+      video.pause();
+    });
+  });
+
+  //card sliders
+  let cardContainers = [...document.querySelectorAll('.card-container')];
+  
